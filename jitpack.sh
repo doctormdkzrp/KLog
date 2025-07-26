f6c202fd090c7f40c25b793b0ebe6670b740df5e
ZRP <doctormdkzrp@hotmail.com>
2025-07-26 16:52:29 +0800

    fix

--- jitpack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# JitPack build script
-# This file is used by JitPack to build the project
-
-# JitPack will run this script during the build process
-# Make sure the library module is built and published
-
-./gradlew :library:build :library:publishToMavenLocal
